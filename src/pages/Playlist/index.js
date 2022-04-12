fd95f3a77713230afccb35432de560a0524d4e5f
Breno Colaço <brenocolaco@unifor.br>
2022-04-12 17:06:28 -0300

    asdf

--- src/pages/Playlist/index.js
+++ src/pages/Playlist/index.js
@@ -8,17 +8,11 @@ import picture from "../../assets/Zoro.png";
 import Layout from "../../components/Layout";
 import { useNavigate } from "react-router-dom";
 export default function Playlist() {
-  const navigate = useNavigate();
   return (
     <Layout>
       <ContentArea>
         <CardArea>
-          <SongCard
-            onClick={() => navigate("/home")}
-            picture={picture}
-            title="Ronin"
-            artist="Zoro"
-          ></SongCard>
+          <SongCard picture={picture} title="Ronin" artist="Zoro"></SongCard>
         </CardArea>
       </ContentArea>
     </Layout>
